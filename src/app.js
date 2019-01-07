3eccaaa287b6b99f7bdbe64b818ede78cc214a65
SnowSurfer <drinkaziu@gmail.com>
2019-01-07 14:37:01 -0500

    initial commit

---
+++ src/app.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+const ExpenseDashboardPage = () => (
+  <div>
+    This is the Dashboard Page
+  </div>
+);
+
+
+const AddExpensePage = () => (
+  <div>
+    This is the Add Expense Page
+  </div>
+);
+
+const EditExpensePage = () => (
+  <div>
+    This is the Edit Expense Page
+  </div>
+);
+
+const HelpPage = () => (
+  <div>
+    This is the Help Page
+  </div>
+);
+
+const NotFoundPage = () => (
+  <div>
+    404!
+  </div>
+);
+
+const routes = (
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={ExpenseDashboardPage} exact={true} />
+      <Route path="/create" component={AddExpensePage} />
+      <Route path="/edit" component={EditExpensePage} />
+      <Route path="/help" component={HelpPage} />
+      <Route component={NotFoundPage} />
+    </Switch>
+  </BrowserRouter>
+)
+
+ReactDOM.render(routes, document.getElementById('app'));
